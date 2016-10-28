9a7f1ad55f0c195a3f532769244ae1f86bfaa635
eebasadre <edsil@piktochart.com>
2016-10-28 13:16:04 +0800

    Fix error when sign in in facebook

--- SerbisYou-App/ViewController.swift
+++ SerbisYou-App/ViewController.swift
@@ -13,10 +13,11 @@ import FirebaseAuth
 class ViewController: UIViewController, FBSDKLoginButtonDelegate {
    let loginButton = FBSDKLoginButton()
 
+    @IBOutlet weak var LoadinSpinner: UIActivityIndicatorView!
     override func viewDidLoad() {
         super.viewDidLoad()
         self.loginButton.hidden = true
-      
+
         FIRAuth.auth()?.addAuthStateDidChangeListener { auth, user in
           if let user = user {
             let mainStoryboard: UIStoryboard = UIStoryboard(name: "Main", bundle: nil)
@@ -27,7 +28,7 @@ class ViewController: UIViewController, FBSDKLoginButtonDelegate {
             self.loginButton.readPermissions = ["public_profile", "email", "user_friends"]
             self.loginButton.delegate = self
             self.view!.addSubview(self.loginButton)
-            
+
             self.loginButton.hidden = false
           }
         }
@@ -41,13 +42,24 @@ class ViewController: UIViewController, FBSDKLoginButtonDelegate {
 
 
    func loginButton(loginButton: FBSDKLoginButton!, didCompleteWithResult result: FBSDKLoginManagerLoginResult!, error: NSError!) {
+
+    self.loginButton.hidden = true
+    self.LoadinSpinner.startAnimating()
+
+    if(error != nil || result.isCancelled) {
+      self.loginButton.hidden = false
+      self.LoadinSpinner.stopAnimating()
+    } else {
       let credential = FIRFacebookAuthProvider.credentialWithAccessToken(FBSDKAccessToken.currentAccessToken().tokenString)
-   
+
       FIRAuth.auth()?.signInWithCredential(credential) { (user, error ) in
-         print( "Sign in using firebase" )
+        print( "Sign in using firebase" )
       }
-   }
-   
+
+    }
+
+  }
+
    func loginButtonDidLogOut(loginButton: FBSDKLoginButton!) {
       print("User Logout")
    }
